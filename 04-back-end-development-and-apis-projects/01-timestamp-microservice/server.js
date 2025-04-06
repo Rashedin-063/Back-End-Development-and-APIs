38be05be4336d549d7604ad4ad6c19bf4ddcc099
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-06 17:37:12 +0600

    project 2 done

--- 04-back-end-development-and-apis-projects/01-timestamp-microservice/server.js
+++ 04-back-end-development-and-apis-projects/01-timestamp-microservice/server.js
@@ -23,10 +23,8 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get('/api', (req, res) => {
-  const now = new Date();
-  res.json({ unix: now.getTime(), utc: now.toUTCString() });
-});
+
+
 
 app.get('/api/:date', (req, res) => {
   const paramsDate = req.params.date;
@@ -40,6 +38,7 @@ app.get('/api/:date', (req, res) => {
     ? res.json({ error: invalidDate })
     : res.json({ unix: date.valueOf(), utc: date.toUTCString() });
 });
+
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
