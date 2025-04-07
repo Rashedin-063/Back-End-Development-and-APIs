5117bc1721f29f99bc5092d619eddd2aa30ef445
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-07 09:06:27 +0600

    project 1 re-completed

--- 04-back-end-development-and-apis-projects/01-timestamp-microservice/index.js
+++ 04-back-end-development-and-apis-projects/01-timestamp-microservice/index.js
@@ -1,19 +1,12 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
+
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// home route
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -23,7 +16,19 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/:date', (req, res) => {
+  const paramsDate = req.params.date;
+  const invalidDate = 'Invalid Date';
+  const date = paramsDate > 1000 ? new Date (parseInt(paramsDate)) : new Date(paramsDate);
 
+
+  console.log(new Date(parseInt(paramsDate)));
+  
+
+   date.toString() === invalidDate
+     ? res.json({ error: invalidDate })
+     : res.json({ unix: date.valueOf(), utc: date.toUTCString() });
+})
 
 
 // Listen on port set in environment variable or default to 3000
